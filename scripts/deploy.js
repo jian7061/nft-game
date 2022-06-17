074d437592a0b09c04acc7c0071061a7eb1fa792
Jian Kim <jian7061@gmail.com>
2022-06-17 15:50:44 +0900

    update sol version for compatibility with ERC721

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -17,16 +17,22 @@ async function main() {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
   const gameContract = await gameContractFactory.deploy(
     ["Leo", "Aang", "Pikachu"], // Names
-    ["https://i.imgur.com/pKd5Sdk.png", // Images
-    "https://i.imgur.com/xVu4vFL.png", 
-    "https://i.imgur.com/WMB6g9u.png"],
-    [100, 200, 300],                    // HP values
-    [100, 50, 25]                       // Attack damage values
+    [
+      "https://i.imgur.com/pKd5Sdk.png", // Images
+      "https://i.imgur.com/xVu4vFL.png",
+      "https://i.imgur.com/WMB6g9u.png",
+    ],
+    [100, 200, 300], // HP values
+    [100, 50, 25] // Attack damage values
   );
 
   await gameContract.deployed();
 
   console.log("Game contract deployed to:", gameContract.address);
+
+  let txn;
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
