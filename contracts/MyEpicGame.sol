22d6b8e9c7cc41a319af30c98eb9b2d5f4dad8bd
Jian Kim <jian7061@gmail.com>
2022-06-17 15:44:38 +0900

    add mint NFT function

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,9 +1,14 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
 
-contract MyEpicGame {
+contract MyEpicGame is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     struct CharacterAttributes {
         uint characterIndex;
@@ -15,12 +20,18 @@ contract MyEpicGame {
     }
     CharacterAttributes[] defaultCharacters;
 
+    mapping (uint256 => CharacterAttributes) tokenIdToAttributes;
+
+    mapping (address => uint256) ownerToTokenId;
+
     constructor(
         string[] memory characterNames,
         string[] memory characterImageURIs,
         uint[] memory characterHp,
         uint[] memory characterAttackDmg
-    ) {
+    )
+    ERC721("Heroes","HERO")
+     {
         for(uint i = 0; i < characterNames.length; i++) {
             defaultCharacters.push(CharacterAttributes({
                 characterIndex : i,
@@ -33,7 +44,26 @@ contract MyEpicGame {
             CharacterAttributes memory c = defaultCharacters[i];
             console.log("Done initializing %s w'HP %s, img %s", c.name, c.hp, c.imageURI);
         }
+        _tokenIds.increment();
     }
 
-    
+    function mintCharacterNFT(uint _characterIndex) external {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        tokenIdToAttributes[newItemId] = CharacterAttributes({
+                characterIndex : _characterIndex,
+                name : defaultCharacters[_characterIndex].name,
+                imageURI : defaultCharacters[_characterIndex].imageURI,
+                hp : defaultCharacters[_characterIndex].hp,
+                maxHp : defaultCharacters[_characterIndex].maxHp,
+                attackDamage : defaultCharacters[_characterIndex].attackDamage
+            });
+        console.log("Minted NFT with tokenId %s, characterIndex %s", newItemId, _characterIndex);
+
+        ownerToTokenId[msg.sender] = newItemId;
+
+        _tokenIds.increment();
+    }
 }
