6d5426a1190c3e0907198f37b1edfbf9919f4e3a
Jian Kim <jian7061@gmail.com>
2022-06-17 15:29:11 +0900

    init & create MyEpicGame contract

---
+++ scripts/deploy.js
@@ -0,0 +1,39 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy(
+    ["Leo", "Aang", "Pikachu"], // Names
+    ["https://i.imgur.com/pKd5Sdk.png", // Images
+    "https://i.imgur.com/xVu4vFL.png", 
+    "https://i.imgur.com/WMB6g9u.png"],
+    [100, 200, 300],                    // HP values
+    [100, 50, 25]                       // Attack damage values
+  );
+
+  await gameContract.deployed();
+
+  console.log("Game contract deployed to:", gameContract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
